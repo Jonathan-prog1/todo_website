8382a832bcac6d7d91eefa9d87048bb2310f5f18
Jonathan-prog1 <jmcapp99@outlook.com>
2026-01-07 21:01:28 -0700

    setup the database for todos.

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Todo, TodoCatagory
 
 # Register your models here.
+admin.site.register(Todo)
+admin.site.register(TodoCatagory)
--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TodoCatagory(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    header = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.header
+
+class Todo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    catagory = models.ForeignKey(TodoCatagory, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
